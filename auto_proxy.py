c0da313e2c44a1c76fbe83578c73055dda3142da
Mustard404 <mustard_404@163.com>
2022-04-16 22:50:43 +0800

    main

--- auto_proxy.py
+++ auto_proxy.py
@@ -1,71 +1,59 @@
-import winreg
-import ctypes
-import requests
-import time
-import json
-
-
-# API设置
-py_cn_api = "http://tiqu.pyhttp.taolop.com/getip?count=1&neek=XXXX&type=2&yys=0&port=2&sb=&mr=1&sep=0&ts=1&time=2"
+import yaml, json, requests, datetime, time
+# 测试封禁地址
 test_url = "https://sec404.cn/"
-
-# 如果从来没有开过代理 有可能健不存在 会报错
-INTERNET_SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                                   r'Software\Microsoft\Windows\CurrentVersion\Internet Settings',
-                                   0, winreg.KEY_ALL_ACCESS)
-# 设置刷新
-INTERNET_OPTION_REFRESH = 37
-INTERNET_OPTION_SETTINGS_CHANGED = 39
-internet_set_option = ctypes.windll.Wininet.InternetSetOptionW
-
-
-def set_key(name, value):
-    _, reg_type = winreg.QueryValueEx(INTERNET_SETTINGS, name)
-    winreg.SetValueEx(INTERNET_SETTINGS, name, 0, reg_type, value)
-
-# 启用代理
-def start(proxy_server):
-    set_key('ProxyEnable', 1) 
-    set_key('ProxyOverride', u'*.local;<local>') # 绕过本地
-    set_key('ProxyServer', proxy_server) 
-    internet_set_option(0, INTERNET_OPTION_REFRESH, 0, 0)
-    internet_set_option(0,INTERNET_OPTION_SETTINGS_CHANGED, 0, 0)
-# 停用代理
-
-def stop():
-    set_key('ProxyEnable', 0)
-    internet_set_option(0, INTERNET_OPTION_REFRESH, 0, 0)
-    internet_set_option(0, INTERNET_OPTION_SETTINGS_CHANGED, 0, 0)
-    print('代理已关闭')
-
-# 防止代理开启干扰导致接口白名单错误
-
-
 # 品易API
-def py_cn():
-    stop()
-    proxy = json.loads(requests.get(py_cn_api).text)['data'][0]
-    proxyMeta = "http://%(host)s:%(port)s" % {
-        "host" : proxy['ip'],
-        "port" : proxy['port'],
-    }
-    proxies = {
-        "http" : proxyMeta,
-        "https" : proxyMeta
-    }
-    start(str(proxy['ip']) + ':' + str(proxy['port']))
-    print("已设置当前IP为:" + proxy['ip'] )
-    return proxies
+py_api = "http://tiqu.pyhttp.taolop.com/getip?count=10&neek=XXXXX&type=2&yys=0&port=11&sb=&mr=1&sep=0&ts=1&ys=1&cs=1&regions=XXXXXX&time=4"
+# 设置错误连接次数
+max_connect_error = 3
 
-proxy_status = py_cn()
+def make_yaml():
+    # 读取配置模版
+    stream = open('Auto_proxy_example.yaml', 'r')
+    clash_conf = yaml.load(stream, Loader=yaml.CLoader)
+    stream.close()
+    # 请求API，并添加品易返回S5代理
+    py_proxy = json.loads(requests.get(py_api).text)
+    clash_conf['proxies'] = []
+    clash_conf['proxy-groups'][0]['proxies'] = []
+    clash_conf['proxy-groups'][1]['proxies'] = []
+    for proxy_data in py_proxy['data']:
+        proxy = {}
+        proxy['name'] = proxy_data['isp'] + "_" + proxy_data['city'] + "_" + proxy_data['ip']
+        proxy['type'] = "socks5"
+        proxy['server'] = proxy_data['ip']
+        proxy['port'] = proxy_data['port']
+        clash_conf['proxies'].append(proxy)
+        clash_conf['proxy-groups'][0]['proxies'].append(proxy['name'])
+        clash_conf['proxy-groups'][1]['proxies'].append(proxy['name'])
+    clash_conf['proxy-groups'][0]['url'] = test_url
+    clash_conf['proxy-groups'][1]['url'] = test_url
+    # 生成新配置文件
+    stream = open('Auto_proxy.yaml', 'w', encoding="utf-8")
+    yaml.safe_dump(clash_conf, stream, allow_unicode=True)
+    stream.close()
 
+def time():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# 初始化获取ip代理
+make_yaml()
+# 初始化错误连接数
+connect_error = 0
+# 测试是否封禁
 while True:
-    # 设置刷新
     try:
         # 判断代理IP是否被封禁,可根据需求设置timeout值
-        resp=requests.get(test_url, proxies=proxy_status, timeout=10)
-        print("OK," + str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))))
-
+        resp = requests.get(test_url, timeout=10)
+        get_ip = json.loads(requests.get("https://ip.cn/api/index?ip=&type=0", timeout=10).text)
+        print( str(time())  + "，OK，公网IP："  + get_ip['ip'] )
+        connect_error = 0
     except:
-        print("IP已被封禁，停止代理")
-        proxy_status = py_cn()
+        connect_error = connect_error + 1
+        print( str(time()) + "，连接错误次数:" + str(connect_error))
+        # 自定义修改错误连接次数
+        if connect_error == max_connect_error:
+            make_yaml()
+            print( str(time())  + "，IP已被封禁，重新获取代理")
+            connect_error = 0
+            time.sleep(5)
+
+        
